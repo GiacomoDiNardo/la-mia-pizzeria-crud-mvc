b1bfc21d239a9b5c0bfc0ac20746b1551e4ad689
GiacomoDiNardo <vest_95@hotmail.it>
2022-11-22 17:45:29 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,114 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        PizzeriaDbContext db;
+
+        public PizzaController() : base ()
+        {
+            db = new PizzeriaDbContext();
+        }
+
+        public IActionResult Index()
+        {
+            
+            List<Pizza> listaPizze = db.Pizze.ToList();
+
+
+            return View(listaPizze);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            PizzeriaDbContext db = new PizzeriaDbContext();
+
+            Pizza pizza = db.Pizze.Where(p => p.PizzaId == id).FirstOrDefault();
+            return View(pizza);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            db.Pizze.Add(pizza);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Update(int id)
+        {
+            Pizza pizza = db.Pizze.Where(pizza => pizza.PizzaId == id).FirstOrDefault();
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            return View(pizza);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza formData)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            Pizza pizza = db.Pizze.Where(pizza => pizza.PizzaId == id).FirstOrDefault();
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            pizza.Name = formData.Name;
+            pizza.Description = formData.Description;
+            pizza.Image = formData.Image;
+            pizza.Prezzo = formData.Prezzo;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            Pizza pizza = db.Pizze.Where(post => post.PizzaId == id).FirstOrDefault();
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            db.Pizze.Remove(pizza);
+            db.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
